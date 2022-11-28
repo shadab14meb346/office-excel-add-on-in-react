797014cafc4b90338baf072e7df0e11b408500ec
shadab <shadab14meb346@gmail.com>
2022-11-28 16:59:05 +0530

    send segments in schema response

--- shadab-office-excel-add-on-reactjs/backend/ga-methods.js
+++ shadab-office-excel-add-on-reactjs/backend/ga-methods.js
@@ -25,15 +25,40 @@ const getAccountSummaries = async (token) => {
     console.log(e);
   }
 };
+const getSegments = async (token) => {
+  const requestEndpoint = `${BASE_URL}/management/segments`;
+  const requestOptions = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  try {
+    const res = await fetch(requestEndpoint, requestOptions);
+    const data = await res.json();
+    const formattedData = data.items.map((item) => {
+      return {
+        definition: item.definition,
+        id: item.segmentId,
+        name: item.name,
+        type: item.type,
+      };
+    });
+    return formattedData;
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 const getSchema = async (request, response) => {
   const token = request.headers["authorization"]?.split(" ")[1];
   if (!token) {
     response.status(400).send("Not Authorized");
   }
-  const accountSummaries = await getAccountSummaries(token);
+  const [accountSummaries, segments] = await Promise.all([getAccountSummaries(token), getSegments(token)]);
   const schema = {
     accounts: accountSummaries,
+    segments: segments,
   };
   response.json({
     data: schema,
